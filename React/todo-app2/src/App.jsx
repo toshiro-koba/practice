d971b0317c3a1c84873e7213a44c281561629d3b
toshiro-koba <kobato.231.0625@gmail.com>
2022-08-27 09:28:51 +0900

    todo アプリの雛形を作成 #6

--- React/todo-app2/src/App.jsx
+++ React/todo-app2/src/App.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css'
 
 export const App = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [incompleteTodos, setIncompleteTodos] = useState(['a', 'b'])
+  // eslint-disable-next-line no-unused-vars
+  const [completeTodos, setCompleteTodos] = useState(['A', 'B'])
   return (
     <>
-      <h1>hello</h1>
-      <p>body</p>
+      <div className='input-area'>
+        <input placeholder='TODOを入力' />
+        <button>追加</button>
+      </div>
+
+      <div className='incomplete-area'>
+        <p className='title'>未完了のTODO</p>
+        <ul>
+          {incompleteTodos.map(todo => {
+            return (
+              <div key={todo} className='list-row'>
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            )
+          })}
+        </ul>
+      </div>
+
+      <div className='complete-area'>
+        <p className='title'>完了のTODO</p>
+        <ul>
+          {completeTodos.map(todo => {
+            return (
+              <div key={todo} className='list-row'>
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            )
+          })}
+        </ul>
+      </div>
     </>
   )
 }
