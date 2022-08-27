9f02219616e4e47b26eeaf0e193940e4f5a3555d
toshiro-koba <kobato.231.0625@gmail.com>
2022-08-27 10:07:42 +0900

    タスク追加と削除機能を実装 #6

--- React/todo-app2/src/App.jsx
+++ React/todo-app2/src/App.jsx
@@ -2,26 +2,43 @@ import React, { useState } from 'react'
 import './App.css'
 
 export const App = () => {
-  // eslint-disable-next-line no-unused-vars
+  const [todoText, setTodoText] = useState('')
   const [incompleteTodos, setIncompleteTodos] = useState(['a', 'b'])
   // eslint-disable-next-line no-unused-vars
   const [completeTodos, setCompleteTodos] = useState(['A', 'B'])
+
+  const onChangeTodoText = e => setTodoText(e.target.value)
+
+  const onClickAdd = () => {
+    if (todoText === '') return
+    const newTodos = [...incompleteTodos, todoText]
+    setIncompleteTodos(newTodos)
+    setTodoText('')
+  }
+
+  const onClickDelete = index => {
+    const deleteCount = 1
+    const newTodos = [...incompleteTodos]
+    newTodos.splice(index, deleteCount)
+    setIncompleteTodos(newTodos)
+  }
+
   return (
     <>
       <div className='input-area'>
-        <input placeholder='TODOを入力' />
-        <button>追加</button>
+        <input placeholder='TODOを入力' value={todoText} onChange={onChangeTodoText} />
+        <button onClick={onClickAdd}>追加</button>
       </div>
 
       <div className='incomplete-area'>
         <p className='title'>未完了のTODO</p>
         <ul>
-          {incompleteTodos.map(todo => {
+          {incompleteTodos.map((todo, index) => {
             return (
-              <div key={todo} className='list-row'>
+              <div key={index} className='list-row'>
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             )
           })}
@@ -31,12 +48,11 @@ export const App = () => {
       <div className='complete-area'>
         <p className='title'>完了のTODO</p>
         <ul>
-          {completeTodos.map(todo => {
+          {completeTodos.map((todo, index) => {
             return (
-              <div key={todo} className='list-row'>
+              <div key={index} className='list-row'>
                 <li>{todo}</li>
-                <button>完了</button>
-                <button>削除</button>
+                <button>戻す</button>
               </div>
             )
           })}
